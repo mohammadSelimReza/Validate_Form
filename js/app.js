1ee6ff05f95432e0f7b6310a9023ff914886f40d
Selim Reza <srreza1999@gmail.com>
2022-03-07 10:25:51 +0600

    doing js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
 // displaying Error massage::
-function displayError(errId, errorMsg) {
-    document.getElementById(errId).innerText = errorMsg;
-}
+const displayError = (errId, errorMsg) => document.getElementById(errId).innerText = errorMsg;
+// values of client::
+const valueId = Id => document.getElementById(Id).value;
+// define a function to validate form::
+document.getElementById('btn').addEventListener('click', function() {
+    const nameValue = valueId('name');
+    const emailValue = valueId('email');
+    const numValue = valueId('num');
+    const countryName = valueId('country');
+    const genderValue = valueId('gender');
+    console.log(genderValue, countryName);
+    const hobbies = [];
+});
